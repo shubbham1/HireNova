093f28308a6dee12bd62c65d259d22e7bae80c34
Shubham Yadav <shubhamyadav1679@gmail.com>
2026-06-12 00:00:31 +0530

    Fix auth imports

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Login from "./components/auth/Login";
-import Signup from "./components/auth/Signup";
+import Login from "./components/auth/login";
+import Signup from "./components/auth/signup";
 import Home from "./components/Home";
 import Jobs from './components/Jobs';
 import Browse from './components/Browse';
