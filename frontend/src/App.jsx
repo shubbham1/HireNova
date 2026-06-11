0c2cff234ed736af8afd5b4a3993413ba2a213b5
Shubham Yadav <shubhamyadav1679@gmail.com>
2026-06-11 23:49:50 +0530

    Fix import case

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,7 +7,7 @@ import Jobs from './components/Jobs';
 import Browse from './components/Browse';
 import Profile from './components/Profile';
 import JobDescription from './components/JobDescription';
-import Companies from './components/admin/companies';
+import Companies from './components/admin/Companies';
 import CompanyCreate from './components/admin/CompaniesCreate';
 import CompanySetup from './components/admin/CompanySetup';
 import ProtectedRoute from './components/admin/ProtectedRoute';
