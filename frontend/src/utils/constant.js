8a09f728c92a11473e17cf7be182740bcb7d3398
Shubham Yadav <shubhamyadav1679@gmail.com>
2026-06-11 23:19:25 +0530

    Update API URLs for production

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,5 +1,5 @@
-export const USER_API_POINT="http://localhost:8000/api/v1/user";
-export const JOB_API_POINT="http://localhost:8000/api/v1/job";
-export const APPLICANT_API_POINT ="http://localhost:8000/api/v1/application";
-export const COMPANY_API_POINT = "http://localhost:8000/api/v1/company";
-export const APPLICATION_API_POINT ="http://localhost:8000/api/v1/application";
+export const USER_API_POINT="https://hirenova-3fbc.onrender.com/api/v1/user";
+export const JOB_API_POINT="https://hirenova-3fbc.onrender.com/api/v1/job";
+export const APPLICANT_API_POINT="https://hirenova-3fbc.onrender.com/api/v1/application";
+export const COMPANY_API_POINT="https://hirenova-3fbc.onrender.com/api/v1/company";
+export const APPLICATION_API_POINT="https://hirenova-3fbc.onrender.com/api/v1/application";
